d56b4ee26767056f33fcce92c6e0bf60e1ecdb96
Sebastien Joseph <sebastienjoseph07@outlook.com>
2024-03-02 08:27:14 -0500

    Updated ProductController

--- src/main/java/com/shopsmart/productservice/controller/ProductController.java
+++ src/main/java/com/shopsmart/productservice/controller/ProductController.java
@@ -1,16 +1,21 @@
 package com.shopsmart.productservice.controller;
 
 import com.shopsmart.productservice.dto.ProductRequest;
+import com.shopsmart.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest) {
-
+        productService.createProduct(productRequest);
     }
 }
