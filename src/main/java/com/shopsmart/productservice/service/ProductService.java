d14cee53f46d29a1abb89375632b1ca600636236
Sebastien Joseph <sebastienjoseph07@outlook.com>
2024-03-02 08:23:29 -0500

    Updated ProductService

--- src/main/java/com/shopsmart/productservice/service/ProductService.java
+++ src/main/java/com/shopsmart/productservice/service/ProductService.java
@@ -1,4 +1,23 @@
 package com.shopsmart.productservice.service;
 
+import com.shopsmart.productservice.dto.ProductRequest;
+import com.shopsmart.productservice.model.Product;
+import com.shopsmart.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+    }
+
 }
