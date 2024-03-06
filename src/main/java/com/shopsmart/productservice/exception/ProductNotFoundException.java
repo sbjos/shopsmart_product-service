4a428d3983c3b9c3ced5003a289e54e0e74f428e
Sebastien Joseph <sebastienjoseph07@outlook.com>
2024-03-06 10:39:15 -0500

    Serialized ProductNotFoundException class

--- src/main/java/com/shopsmart/productservice/exception/ProductNotFoundException.java
+++ src/main/java/com/shopsmart/productservice/exception/ProductNotFoundException.java
@@ -1,12 +1,17 @@
 package com.shopsmart.productservice.exception;
 
+import java.io.Serial;
+
 public class ProductNotFoundException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 3811768928456940938L;
 
     public ProductNotFoundException() {}
 
     public ProductNotFoundException(String message) {
         super(message);
     }
+
     public ProductNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
@@ -15,11 +20,10 @@ public class ProductNotFoundException extends RuntimeException {
         super(cause);
     }
 
-    public ProductNotFoundException(String message, Throwable cause,
-                                       boolean enableSuppression,
-                                       boolean writableStackTrace) {
+    public ProductNotFoundException(String message,
+                                    Throwable cause,
+                                    boolean enableSuppression,
+                                    boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
-
-
 }
